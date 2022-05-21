dc778c402f5684610d6263fef946ca08af3dbc20
calvinscharf <scharfcalvin@gmail.com>
2022-05-20 19:34:11 -0500

    added javascript to make game winnable and reset

--- scripts.js
+++ scripts.js
@@ -7,6 +7,11 @@
     // next to each @TODO you will find tasks that need to be finished
 
 // The variable will change from X to O based on what player turn it is. We need to hold this so we can place an X or O on the board when they're clicked.
+const board = [
+  ["","", ""],
+  ["","", ""],
+  ["","", ""]
+];
 let currentMarker = 'X'
 
 
@@ -49,9 +54,13 @@ const addMarker = (id) => {
   // = currentMarker
   // .getElementById(id)
   // document
-  // .innerHTML 
-
-  changeMarker()
+  // .innerHTML
+  const row = parseInt(id.charAt(0))
+  const column = parseInt(id.charAt(2))
+  
+  board[row][column] = currentMarker
+  console.log(board[row][column],"board",board)
+  checkForWin()
 }
 
 
@@ -72,18 +81,16 @@ const changeMarker = () => {
   }
 }
 
-
-
-
-
-
-
-
-
-
 // This "resetBoard" function is called when the user clicks on the "Restart" button.
 const resetBoard = () => {
-  
+  // board = [
+    // ["","", ""],
+    // ["","", ""],
+    // ["","", ""]
+  // ];
+  for(let i = 0; i < board.length; i++){
+    board[i]=""
+  }
   // @TODO-3: To make your "Restart" button work you'll need to build a line of code here that:
       // collects all of the "td" elements into an HTML Collection: https://www.w3schools.com/jsref/dom_obj_htmlcollection.asp  
     
@@ -104,3 +111,41 @@ const resetBoard = () => {
     squares[i].innerHTML = null
   }  
 }
+
+
+
+const checkForWin = () => {
+  if(horizontalWin() || verticalWin() || diagonalWin()) {
+    window.alert(`Player ${currentMarker} won!`)
+  } else {
+    changeMarker()
+  } 
+}
+
+
+const horizontalWin = () =>{
+if((board[0][0] == "X" && board[0][1] == "X" && board[0][2] == "X") 
+|| (board[0][0] == "O" && board[0][1] == "O" && board[0][2] == "O")
+|| (board[1][0] == "X" && board[1][1] == "X" && board[1][2] == "X")
+|| (board[1][0] == "O" && board[1][1] == "O" && board[1][2] == "O")
+|| (board[2][0] == "X" && board[2][1] == "X" && board[2][2] == "X")
+|| (board[2][0] == "O" && board[2][1] == "O" && board[2][2] == "O")
+)
+{ console.log("horizontalWin")
+  return true;
+}
+}
+
+const verticalWin = () =>{
+if((board[0][0] == "X" && board[1][0] == "X" && board[2][0] == "X") 
+|| (board[0][0] == "O" && board[1][0] == "O" && board[2][0] == "O")
+){
+  return true;
+}
+}
+ const diagonalWin = () => {
+if((board[0][0] == "X" && board[1][1] == "X" && board[2][2] == "X") 
+|| (board[0][0] == "O" && board[1][1] == "O" && board[2][2] == "O")
+){
+  return true;
+}}
