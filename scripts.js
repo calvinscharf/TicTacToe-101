fe7c68e64a5a8c14bb5d7be4267137f0401804dd
calvinscharf <scharfcalvin@gmail.com>
2022-05-21 12:49:59 -0500

    changes Xs and Os in js

--- scripts.js
+++ scripts.js
@@ -137,15 +137,23 @@ if((board[0][0] == "X" && board[0][1] == "X" && board[0][2] == "X")
 }
 
 const verticalWin = () =>{
+  console.log("verticalWin")
 if((board[0][0] == "X" && board[1][0] == "X" && board[2][0] == "X") 
 || (board[0][0] == "O" && board[1][0] == "O" && board[2][0] == "O")
-){
+|| (board[0][1] == "X" && board[1][1] == "X" && board[2][1] == "X")
+|| (board[0][1] == "O" && board[1][1] == "O" && board[2][1] == "O")
+|| (board[0][2] == "X" && board[1][2] == "X" && board[2][2] == "X")
+|| (board[0][2] == "O" && board[1][2] == "O" && board[2][2] == "O")
+)
+{  console.log("verticalWin")
   return true;
 }
 }
  const diagonalWin = () => {
 if((board[0][0] == "X" && board[1][1] == "X" && board[2][2] == "X") 
 || (board[0][0] == "O" && board[1][1] == "O" && board[2][2] == "O")
+|| (board[2][0] == "X" && board[1][1] == "X" && board[2][0] == "X")
+|| (board[2][0] == "O" && board[1][1] == "O" && board[2][0] == "O")
 ){
   return true;
 }}
